1658f98068cd5c78bbb71742f8baf87642d12138
github username <github email address>
2019-04-15 17:22:20 -0700

    added basic methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,30 @@
 class User < ApplicationRecord
   has_many :donations_made, :class_name => "Donation", :foreign_key => "sender_id"
-  has_many :donations_recieved, :class_name => "Donation", :foreign_key => "receiver_id"
+  has_many :donations_received, :class_name => "Donation", :foreign_key => "receiver_id"
 
   has_many :messages_sent, :class_name => "Message", :foreign_key => "sender_id"
-  has_many :messages_recieved, :class_name => "Message", :foreign_key => "receiver_id"
+  has_many :messages_received, :class_name => "Message", :foreign_key => "receiver_id"
 
   has_many :reviews_written, :class_name => "Review", :foreign_key => "writer_id"
   has_many :reviews, :class_name => "Review", :foreign_key => "user_id"
+
+  has_many :notifications
+
+  def new_donation(receiver:, amount:)
+    donation = Donation.create(sender: self, receiver: receiver, amount: amount)
+    notification = Notification.create(user: receiver, content: donation)
+    donation
+  end
+
+  def new_message(receiver:, title:, content:)
+    message = Message.create(sender: self, receiver: receiver, title: title, content: content)
+    notification = Notification.create(user: receiver, content: message)
+    message
+  end
+
+  def new_review(user:, rating:, content:)
+    review = Review.create(writer: self, user: user, rating: rating, content: content)
+    notification = Notification.create(user: receiver, content: review)
+    review
+  end
 end
